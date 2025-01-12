eb0564ce001bd91780a87752e8c21c49cafc9d18
Konstantinos Panayiotou <klpanagi@gmail.com>
2025-01-12 02:50:36 +0200

    Remove goal-dsl directory if previously exists

--- setup_deps.sh
+++ setup_deps.sh
@@ -5,5 +5,6 @@ pip install https://github.com/robotics-4-all/goalee/archive/devel.zip -U
 
 pip install -r requirements.txt
 
+rm -rf goal-dsl 2&1>/dev/null
 git clone https://github.com/robotics-4-all/goal-dsl.git && \
     cd goal-dsl && git checkout devel && pip install . && rm -rf goal-dsl
